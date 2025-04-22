cedd814b2f1148c8a16b0fbef24480f233bb6ebb
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-22 00:31:38 -0600

    agregando src/test/java/steps/object_objectSteps.java

---
+++ src/test/java/steps/object_objectSteps.java
@@ -0,0 +1,22 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.Assert.*;
+
+public class object_objectSteps {
+
+    @Given("object Object scenario setup")
+    public void object_object_scenario_setup() {
+        // Setup code for object Object scenario
+    }
+
+    @When("object Object action is performed")
+    public void object_object_action_is_performed() {
+        // Code to perform object Object action
+    }
+
+    @Then("object Object expected result should be observed")
+    public void object_object_expected_result_should_be_observed() {
+        // Code to verify object Object expected result
+    }
+}
